87d56ae6a4304eb156503edca057e3d6b1042519
Joe Santiago <joseph.santiago@gmail.com>
2022-06-24 19:09:27 -0700

    use Script component

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
+import Script from "next/script";
 
 const About = () => (
-  <p>
-    <Image src="/images/profile.jpg" width={144} height={144} />
-    some <Link href="/">thing</Link> here
-  </p>
+  <>
+    <Head>
+      <title>This is a title</title>
+    </Head>
+    <Script
+      src="https://connect.facebook.net/en_US/sdk.js"
+      strategy="lazyOnload"
+      onLoad={() =>
+        console.log(`script loaded correctly, window.FB has been populated`)
+      }
+    />
+    <p>
+      <Image src="/images/profile.jpg" width={144} height={144} />
+      some <Link href="/">thing</Link> here
+    </p>
+  </>
 );
 
 export default About;
