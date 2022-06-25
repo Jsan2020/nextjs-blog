47440228240886ecfc72fc01181219f3553685dc
Joe Santiago <joseph.santiago@gmail.com>
2022-06-24 22:53:48 -0700

    lame bio

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Some stuff here</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>A web dude that likes tinkering.</p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
