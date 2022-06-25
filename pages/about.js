e9f35e645352afaf89989e87ee49442f5e52be7b
Joe Santiago <joseph.santiago@gmail.com>
2022-06-24 19:03:20 -0700

    add an image

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const About = () => (
   <p>
+    <Image src="/images/profile.jpg" width={144} height={144} />
     some <Link href="/">thing</Link> here
   </p>
 );
